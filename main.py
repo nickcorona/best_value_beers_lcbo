b265ee022d4b68174ba9e9d91da7076286d739f5
Nick Corona <ncorona3@gatech.edu>
2023-06-24 20:01:34 -0400

    change workflow name

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 import dotenv
 
-from constants import TO_EMAIL
-from email_sender import send_email
 from lcbo_ratings import lcbo_ratings
-from utils.env_handler import get_env_var
 from web_scrape import web_scrape_lcbo_store_inventory
 
 
